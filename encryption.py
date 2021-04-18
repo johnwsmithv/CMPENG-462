15daedcab6e7eff5f61f18e704f7ef6016ad0b30
Devyani Patra <36823873+17dpatra@users.noreply.github.com>
2021-04-18 18:33:18 -0400

    encryption upload 2

--- encryption.py
+++ encryption.py
@@ -1,36 +1,41 @@
 import random, sympy, string, math
-#rand = random.randint(176504369492349871292707882299854726329,203114452357252166144042181520660158379)
-#p = sympy.prevprime(rand)
-#q = sympy.nextprime(rand)
 
-e = 3
-p = 53
-q = 59
-#print('p, q', p, q)
-n = p*q
-phi = (p-1)*(q-1)
+def encrypt(alpha):
+    rand = random.randint(20,30)
+    p = sympy.prevprime(rand)
+    q = sympy.nextprime(rand)
 
-#creating list of possible values for e
-#temp = list(range(2, phi))
+    n = p*q
+    phi = (p-1)*(q-1)
 
-#finding small e exponent that is not a factor of n and is less than phi
-i = 0
-e = 2
-while (e < phi):
-    if (math.gcd(e, phi) == 1):
-        break
-    else:
-        e+=1
-print('n, phi, e', n, phi, e) #n and e are public keys
+    #finding small e exponent that is not a factor of n and is less than phi
+    i = 0
+    e = 2
+    while (e < phi):
+        if (math.gcd(e, phi) == 1):
+            break
+        else:
+            e+=1
 
-#alpha = string.ascii_letters
-alpha = 'hi' #input
-concatNum = ""
-for i in alpha.upper():
-    value = ord(i)-64
-    concatNum += str(value)
-print(concatNum)
+    #print('n, phi, e', n, phi, e) #n and e are public keys
 
-c = int(concatNum)**e
-c = c%n #make the number of a constant number - 8    integers = leading zeroes
-print(c)
+    concatNum = []
+    for i in alpha:
+        value = ord(i)
+        value = value**e
+        c = value%n
+        concatNum.append(c)
+    #print(concatNum)
+
+    return [n, e, concatNum]
+    
+
+def main():
+    #take input
+    alpha = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed nulla orci. Donec dignissim magna bibendum libero viverra, in facilisis eros luctus. Vestibulum pulvinar augue et elementum sollicitudin. Mauris cursus feugiat varius. Proin tincidunt ligula nec elit semper, in maximus est semper. Sed commodo mauris ante, et fringilla est pellentesque ut. Vivamus quis dapibus eros. Cras luctus ultricies ipsum, semper pharetra arcu accumsan in. Donec a dui nisl. Donec posuere mauris nulla, vitae blandit arcu vestibulum ut." #input
+    #alpha = input("Enter message: ")
+
+    print(encrypt(alpha)) #gives [n, e, [ciphertext numbers]]
+
+if __name__=="__main__":
+    main()
