67e4369498c3a840b171d16c4b10b39ce3ed39c6
Devyani Patra <36823873+17dpatra@users.noreply.github.com>
2021-04-21 15:44:05 -0400

    encryption upload 3 - cleaned up

--- encryption.py
+++ encryption.py
@@ -1,6 +1,7 @@
 import random, sympy, math
 
-def encrypt(alpha):
+#function for determining public keys
+def getPublicK(alpha):
     rand = random.randint(20,30)
     p = sympy.prevprime(rand)
     q = sympy.nextprime(rand)
@@ -8,8 +9,6 @@ def encrypt(alpha):
     n = p*q
     phi = (p-1)*(q-1)
 
-    #finding small e exponent that is not a factor of n and is less than phi
-    i = 0
     e = 2
     while (e < phi):
         if (math.gcd(e, phi) == 1):
@@ -18,7 +17,11 @@ def encrypt(alpha):
             e+=1
 
     #print('n, phi, e', n, phi, e) #n and e are public keys
-
+    return [n, e]
+    
+def encrypt(alpha):
+    n, e = getPublicK(alpha) #getting public keys
+    
     #doing the actual encryption of each letter using ascii value, e, and n values
     concatNum = []
     for i in alpha:
@@ -30,7 +33,6 @@ def encrypt(alpha):
 
 
     #print(concatNum)
-
 
     #combining 2 letters at a time = each value in finalConcat is a string of length 6
     finalConcat = []
@@ -50,7 +52,6 @@ def encrypt(alpha):
     
 
 def main():
-    #take input
     alpha = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed nulla orci. Donec dignissim magna bibendum libero viverra, in facilisis eros luctus. Vestibulum pulvinar augue et elementum sollicitudin. Mauris cursus feugiat varius. Proin tincidunt ligula nec elit semper, in maximus est semper. Sed commodo mauris ante, et fringilla est pellentesque ut. Vivamus quis dapibus eros. Cras luctus ultricies ipsum, semper pharetra arcu accumsan in. Donec a dui nisl. Donec posuere mauris nulla, vitae blandit arcu vestibulum ut." #input
     #alpha = input("Enter message: ")
 
